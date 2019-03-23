805d00059d82f3756a13815b1bd5c767421cedc0
guyamir <guyami@post.bgu.ac.il>
2019-03-23 10:47:31 +0200

    modified and added vis
    Added modified main (dividing mnist into batches) and files for visdom

--- main.py
+++ main.py
@@ -1,169 +1,209 @@
 import torch
 from torch.optim import lr_scheduler
+import torch.utils.data
+import torch.optim as optim
+import torch.nn.functional as F
 import matplotlib.pyplot as plt
 import pandas as pd
 from nn import treeNet
 from loader import load
 import time
 import copy
+import torchvision
 
-n_epochs = 100
+n_epochs = 2
+batch_size_train = 64
+batch_size_test = 1000
+learning_rate = 0.01
+momentum = 0.5
+log_interval = 10
 
-#load data
-l = load('wine',1,0.2)
-X_train, X_valid, y_train, y_valid = torch.from_numpy(l.X_train).cuda(), torch.from_numpy(l.X_valid).cuda(), torch.from_numpy(l.y_train).cuda(), torch.from_numpy(l.y_valid).cuda()
+random_seed = 1
+torch.backends.cudnn.enabled = True
+torch.manual_seed(random_seed)
+
+# #load data
+# l = load('wine',1,0.2)
+# X_train, X_valid, y_train, y_valid = torch.from_numpy(l.X_train).cuda(), torch.from_numpy(l.X_valid).cuda(), torch.from_numpy(l.y_train).cuda(), torch.from_numpy(l.y_valid).cuda()
+
+train_loader = torch.utils.data.DataLoader(
+  torchvision.datasets.MNIST('./files/', train=True, download=True,
+                             transform=torchvision.transforms.Compose([
+                               torchvision.transforms.ToTensor(),
+                               torchvision.transforms.Normalize(
+                                 (0.1307,), (0.3081,))
+                             ])),
+  batch_size=batch_size_train, shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(
+  torchvision.datasets.MNIST('./files/', train=False, download=True,
+                             transform=torchvision.transforms.Compose([
+                               torchvision.transforms.ToTensor(),
+                               torchvision.transforms.Normalize(
+                                 (0.1307,), (0.3081,))
+                             ])),
+  batch_size=batch_size_test, shuffle=True)
+
+examples = enumerate(train_loader)
+batch_idx, (example_data, example_targets) = next(examples)
+
+# import matplotlib.pyplot as plt
+
+# fig = plt.figure()
+# for i in range(6):
+#   plt.subplot(2,3,i+1)
+#   plt.tight_layout()
+#   plt.imshow(example_data[i][0], cmap='gray', interpolation='none')
+#   plt.title("Ground Truth: {}".format(example_targets[i]))
+#   plt.xticks([])
+#   plt.yticks([])
+# fig
+# plt.show()
 
 # Construct our model by instantiating the class defined above
-model = treeNet(X_train, y_train) #change input and output soon
+
+example_data_vec = example_data.reshape([example_data.shape[0],-1]).cuda()
+
+network = treeNet(example_data_vec, example_targets) #change input and output soon
+network.cuda()
+
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+for i in range(6):
+  plt.subplot(2,3,i+1)
+  plt.tight_layout()
+  plt.imshow(example_data[i][0], cmap='gray', interpolation='none')
+  plt.title("Ground Truth: {}".format(example_targets[i]))
+  plt.xticks([])
+  plt.yticks([])
+fig
 
 #learning rate scheduler:
 lr = 1e-2
-optimizer_ft = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+optimizer = torch.optim.SGD(network.parameters(), lr=learning_rate, momentum=momentum)
 
-# Construct our loss function and an Optimizer. The call to model.parameters()
-# in the SGD constructor will contain the learnable parameters of the two
-# nn.Linear modules which are members of the model.
-criterion = torch.nn.NLLLoss(reduction='mean')
-optimizer = torch.optim.SGD(model.parameters(), lr)
+train_losses = []
+train_counter = []
+test_losses = []
+test_counter = [i*len(train_loader.dataset) for i in range(n_epochs + 1)]
 
-since = time.time()
+def train(epoch):
+  network.train()
+  for batch_idx, (data, target) in enumerate(train_loader):
+    target = target.cuda()
+    data = data.reshape([data.shape[0],-1]).cuda()
+    optimizer.zero_grad()
+    output = network(data)
+    loss = F.nll_loss(output, target)
+    loss.backward(retain_graph=True)
+    optimizer.step()
+    if batch_idx % log_interval == 0:
+        print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+            epoch, batch_idx * len(data), len(train_loader.dataset),
+            100. * batch_idx / len(train_loader), loss.item()))
+        train_losses.append(loss.item())
+        train_counter.append(
+            (batch_idx*64) + ((epoch-1)*len(train_loader.dataset)))
+        torch.save(network.state_dict(), './results/model.pth')
+        torch.save(optimizer.state_dict(), './results/optimizer.pth') 
+    network.dTree.pi = network.dTree.iter_pi(network.dTree.P,network.dTree.pi,network.dTree.mu) #change iterpi to not get or recieve things
+   
 
-best_model_wts = copy.deepcopy(model.state_dict())
-best_acc = 0.0
+def test():
+  network.eval()
+  test_loss = 0
+  correct = 0
+  with torch.no_grad():
+    for data, target in test_loader:
+      target = target.cuda()
+      data = data.reshape([data.shape[0],-1]).cuda()
+      output = network(data)
+      test_loss += F.nll_loss(output, target, size_average=False).item()
+      pred = output.data.max(1, keepdim=True)[1]
+      correct += pred.eq(target.data.view_as(pred)).sum()
+  test_loss /= len(test_loader.dataset)
+  test_losses.append(test_loss)
+  print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+    test_loss, correct, len(test_loader.dataset),
+    100. * correct / len(test_loader.dataset)))
 
-# pat = []
-# pav = []
-for epoch in range(1, n_epochs+1):
+# criterion = torch.nn.NLLLoss(reduction='mean')
+# optimizer = torch.optim.SGD(model.parameters(), lr)
 
-    print('Epoch {}/{}'.format(epoch, n_epochs))
-    print('-' * 10)
+test()
+for epoch in range(1, n_epochs + 1):
+  train(epoch)
+  test()
 
-    for phase in ['train', 'val']:
-        if phase == 'train':
+fig = plt.figure()
+plt.plot(train_counter, train_losses, color='blue')
+plt.scatter(test_counter, test_losses, color='red')
+plt.legend(['Train Loss', 'Test Loss'], loc='upper right')
+plt.xlabel('number of training examples seen')
+plt.ylabel('negative log likelihood loss')
+fig
+
+# since = time.time()
+
+# best_model_wts = copy.deepcopy(model.state_dict())
+# best_acc = 0.0
+
+# # pat = []
+# # pav = []
+# for epoch in range(1, n_epochs+1):
+
+#     print('Epoch {}/{}'.format(epoch, n_epochs))
+#     print('-' * 10)
+
+#     for phase in ['train', 'val']:
+#         if phase == 'train':
             
-            exp_lr_scheduler.step()
-            model.train()  # Set model to training mode
-            x = X_train
-            y = y_train
-        else:
-            model.eval()   # Set model to evaluate mode
-            x = X_valid
-            y = y_valid
+#             exp_lr_scheduler.step()
+#             model.train()  # Set model to training mode
+#             x = X_train
+#             y = y_train
+#         else:
+#             model.eval()   # Set model to evaluate mode
+#             x = X_valid
+#             y = y_valid
 
-        running_loss = 0.0
-        running_corrects = 0
+#         running_loss = 0.0
+#         running_corrects = 0
 
-        # zero the parameter gradients
-        optimizer.zero_grad()
+#         # zero the parameter gradients
+#         optimizer.zero_grad()
 
-        # forward
-        # track history if only in train
-        with torch.set_grad_enabled(phase == 'train'):
-            y_pred = model(x)
-            _, preds = torch.max(y_pred, 1)
-            loss = criterion(y_pred, y)
+#         # forward
+#         # track history if only in train
+#         with torch.set_grad_enabled(phase == 'train'):
+#             y_pred = model(x)
+#             _, preds = torch.max(y_pred, 1)
+#             loss = criterion(y_pred, y)
 
-            # backward + optimize only if in training phase
-            if phase == 'train':
-                model.dTree.pi = model.dTree.iter_pi(model.dTree.P,model.dTree.pi,model.dTree.mu)
-                loss.backward(retain_graph=True)
-                optimizer.step()
+#             # backward + optimize only if in training phase
+#             if phase == 'train':
+#                 model.dTree.pi = model.dTree.iter_pi(model.dTree.P,model.dTree.pi,model.dTree.mu)
+#                 loss.backward(retain_graph=True)
+#                 optimizer.step()
 
-        # statistics
-        running_loss += loss.item() * x.size(0)
-        running_corrects += torch.sum(preds == y)
+#         # statistics
+#         running_loss += loss.item() * x.size(0)
+#         running_corrects += torch.sum(preds == y)
 
-        epoch_loss = running_loss / len(x) ##dataset_sizes[phase]
-        epoch_acc = running_corrects.double() / len(x) #dataset_sizes[phase]
+#         epoch_loss = running_loss / len(x) ##dataset_sizes[phase]
+#         epoch_acc = running_corrects.double() / len(x) #dataset_sizes[phase]
 
-        print('{} Loss: {:.4f} Acc: {:.4f}'.format(
-            phase, epoch_loss, epoch_acc))
+#         print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+#             phase, epoch_loss, epoch_acc))
 
-        # deep copy the model
-        if phase == 'val' and epoch_acc > best_acc:
-            best_acc = epoch_acc
-            best_model_wts = copy.deepcopy(model.state_dict())
+#         # deep copy the model
+#         if phase == 'val' and epoch_acc > best_acc:
+#             best_acc = epoch_acc
+#             best_model_wts = copy.deepcopy(model.state_dict())
 
-    print()
+#     print()
 
-time_elapsed = time.time() - since
-print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
-print('Best val Acc: {:4f}'.format(best_acc))
-
-
-
-
-    # keep track of training and validation loss
-    # train_loss = 0.0
-    # valid_loss = 0.0
-
-    #  # train the model #
-    # model.train()
-    # # clear the gradients of all optimized variables
-    # optimizer.zero_grad()
-    # # forward pass: compute predicted outputs by passing inputs to the model
-    # output = model(x)
-    # # calculate the batch loss
-    # loss = criterion(output, y)
-    # # backward pass: compute gradient of the loss with respect to model parameters
-    # loss.backward()
-    # # perform a single optimization step (parameter update)
-    # optimizer.step()
-    # # update training loss
-    # train_loss += loss.item()*x.size(0)
-    
-    # # model.eval()
-    # # # forward pass: compute predicted outputs by passing inputs to the model
-    # # output = model(X_valid)
-    # # # calculate the batch loss
-    # # loss = criterion(output, y_valid)
-    # # # update average validation loss 
-    # # valid_loss += loss.item()*X_valid.size(0)
-    
-    # # calculate average losses
-    # train_loss = train_loss/len(x)
-    # valid_loss = valid_loss/len(X_valid)
-
-    # print(f'epoch {epoch}, train_loss {train_loss}')#, valid_loss {valid_loss}')#, training accuracy {t_accuracy}')#', validation accuracy {v_accuracy}')
-
-    # model.eval()
-    # # forward pass: compute predicted outputs by passing inputs to the model
-    # output = model(X_valid)
-    # # calculate the batch loss
-    # loss = criterion(output, y_valid)
-    # # update average validation loss 
-    # valid_loss += loss.item()*X_valid.size(0)
-
-    # print(f'valid_loss {valid_loss}')
-
-#     # Forward pass: Compute predicted y by passing x to the model
-#     y_pred = model(x).cuda()
-
-#     _, y_m = y_pred.max(1)
-#     t_accuracy = (y == y_m).sum().item()/len(y)
-
-#     # yv_pred = model(X_valid).cuda()
-    
-#     # _, yv_m = yv_pred.max(1)
-#     # v_accuracy = (y_valid == yv_m).sum().item()/len(y_valid)
-#     pat.append(t_accuracy)
-#     # pav.append(v_accuracy)
-
-#     # Compute and print loss
-#     loss = criterion(torch.log(y_pred), y)
-#     print(f'epoch {epoch}, NLLLoss {loss.item()}, training accuracy {t_accuracy}')#', validation accuracy {v_accuracy}')
-
-#     # Zero gradients, perform a backward pass, and update the weights.
-#     optimizer.zero_grad()
-#     loss.backward(retain_graph=True)
-#     optimizer.step()
-#     model.dTree.pi = model.dTree.iter_pi(model.dTree.P,model.dTree.pi,model.dTree.mu)
-
-# df = pd.DataFrame(pat)
-# df.to_csv (r'a.csv', index = None, header=True)
-
-# print (df)
-# # plt.plot(vat)
-# # plt.plot(pat)
-# # plt.show()
+# time_elapsed = time.time() - since
+# print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+# print('Best val Acc: {:4f}'.format(best_acc))
